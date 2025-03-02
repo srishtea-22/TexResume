d23db338c0447b81e0bea63bf1378acf27ea6970
shristy <shristychaudhary4679@gmail.com>
2025-03-02 11:53:11 +0530

    Test PDF rendering in iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,32 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState("");
-  
-  const fetchApi = async () => {
-    const response = await axios.get("http://localhost:5000");
-    setData(response.data);
-  };
-
-  useEffect(() => {
-    fetchApi();
-  }, []);
+  const [pdfUrl, setPdfUrl] = useState(null);
+  const fetchPdf = async () => {
+    const response = await axios.get("http://localhost:5000/generate-pdf", {
+      responseType: "blob",
+    })
+    const url = URL.createObjectURL(response.data);
+    setPdfUrl(url);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
       <h1>API response:</h1>
-      <p>{data}</p>
+      <button onClick={fetchPdf}>Generate PDF</button>
+      {pdfUrl && (
+        <iframe 
+        src={pdfUrl}
+        width="100%"
+        height="500px"
+        style={{border: "none", marginTop: "20px"}}/>
+      )}
       </div>
     </>
-  )
+  );
 }
 
 export default App
