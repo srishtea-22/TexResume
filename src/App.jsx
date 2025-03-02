868290bca8871b5f432a2b63e218c182154ee28c
shristy <shristychaudhary4679@gmail.com>
2025-03-02 09:32:12 +0530

    Initialize Vite with React and test backend setup

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("");
+  
+  const fetchApi = async () => {
+    const response = await axios.get("http://localhost:5000");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+      <h1>API response:</h1>
+      <p>{data}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
