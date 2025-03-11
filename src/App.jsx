23a90618830360070c55b617b4596d2455624512
shristy <shristychaudhary4679@gmail.com>
2025-03-11 20:53:48 +0530

    add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { PdfTeXEngine, XeTeXEngine, DvipdfmxEngine } from "swiftlatex";
+import Sidebar from "./components/Sidebar";
+import './App.css'
 
 function App() {
-  useEffect(() => {
-    const loadEngine = async () => {
-      try {
-        const pdftex = new PdfTeXEngine();
-        console.log("PdfTeXEngine loaded!", pdftex);
-      } catch (error) {
-        console.error("Error loading PdfTeXEngine:", error);
-      }
-    };
+  const [pdf, setpdf] = useState(null);
 
-    loadEngine();
-  }, []);
+  async function compilePdf() {
+    const engine = new PdfTeXEngine();
+    await engine.loadEngine();
+    
+    engine.writeMemFSFile("main.tex", "\\documentclass{article}\\begin{document}Hello, World!\\end{document}");
+    engine.setEngineMainFile("main.tex");
+    let r = await engine.compileLaTeX();
 
-  return <div>LaTeX Engine Ready!</div>;
+    setpdf(new Blob([r.pdf], {type: "application/pdf"}));
+  }
+  
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>TexResume</h1>
+      </header>
+      <div className="main-body">
+      <aside>
+        <Sidebar />
+      </aside>
+      <main>
+        <form></form>
+        <div className="pdf-wrapper">
+        {pdf && (
+          <iframe
+          src={URL.createObjectURL(pdf)}
+          height="100%"
+          width="500px"
+          />
+        )}
+        </div>
+      </main>
+      </div>
+      <footer className="button-container">
+        <button onClick={compilePdf}>Generate PDF</button>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
