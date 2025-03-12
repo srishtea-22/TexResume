372af0541965fc245cccba18effa00dbac3a170b
shristy <shristychaudhary4679@gmail.com>
2025-03-12 09:51:30 +0530

    add logic for page navigation TODO: fix content rendering

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,49 @@
+import { Route, Routes, NavLink } from "react-router-dom"
+
+import ProfileSection from "./FormSections/ProfileSection"
+import EducationSection from "./FormSections/EducationSection"
+import SkillsSection from "./FormSections/SkillsSection"
+import WorkSection from "./FormSections/WorkSection"
+
+const sidebarData = [
+    {
+        title: "profile",
+        path: "/"
+    },
+    {
+        title: "education",
+        path: "/education"
+    },
+    {
+        title: "skills",
+        path: "/skills"
+    },
+    {
+        title: "work",
+        path: "/work"
+    },
+]
+
 function Sidebar () {
     return (
-        <div>
-            <p>this is sidebar</p>
+        <div className="sidebar-container">
+            <Routes>
+                <Route path="/" element={<ProfileSection />}></Route>
+                <Route path="/education" element={<EducationSection />}></Route>
+                <Route path="/skills" element={<SkillsSection />}></Route>
+                <Route path="/work" element={<WorkSection />}></Route>
+            </Routes>
+            <ul className="nav-list">
+                {sidebarData.map((item, index) => {
+                    return (
+                        <li className="nav-item" key={index}>
+                            <NavLink to={item.path} className={({isActive}) => ["nav-link", isActive ? "active" : null].join(" ")}>
+                                <span>{item.title}</span>
+                            </NavLink>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
